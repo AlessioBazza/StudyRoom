542fee8cc84f840d1665d3a2e6722ba78a373546
Christian Joppi <christianjoppi@MacBook-Adolf.homenet.telecomitalia.it>
2014-11-07 19:13:08 +0100

    models db

--- backend/sito/api/models.py
+++ backend/sito/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Utenti(models.Model):
+        ip_address = models.IPAddressField()
+
+
+class Aule(models.Model):
+        nome = models.CharField(max_length=4)
+        piano = models.IntegerField()
+        dimensione = models.IntegerField()
+        locazione = models.ForeignKey("Stabili")
+
+class Stabili(models.Model):
+        nome = models.CharField(max_length=30)
+
+class Posti(models.Model):
+        timestamp = models.DateTimeField()
+        posti_liberi = models.IntegerField()
+        user = models.ForeignKey("Utenti")
+        aula = models.ForeignKey("Aule")
+
+
