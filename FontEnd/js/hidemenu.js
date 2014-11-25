696f24548974d7a8dbc3dbf531548b407fdef6e6
Filippo Benozzi <filippo@philspark.com>
2014-11-25 15:57:07 +0100

    riordino

--- FontEnd/js/hidemenu.js
+++
@@ -1,9 +0,0 @@
-var main = function(){
-    $('.stat').hide();
-    $('.containerItem').click(function(){
-        $('.stat').hide();
-        $(this).children('.stat').show();
-    }); 
-};
-
-$(document).ready(main);
