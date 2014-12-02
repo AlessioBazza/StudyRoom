05e6b7fa7139a68e1318fa27a36e5bfcdced7090
emilio <emilio.dorigatti@gmail.com>
2014-12-02 15:03:34 +0100

    changed production settings

--- backend/sito/sito/settings/prod.py
+++ backend/sito/sito/settings/prod.py
@@ -1,17 +1,19 @@
 from base import *
 
-# set thes in __init__.py
-# SECRET_KEY = ...
-# DATABASES = { ... }
-
+#SECRET_KEY = '' # set this in __init__.py 
 DEBUG = False
 TEMPLATE_DEBUG = False
-STATIC_ROOT = '/var/www/django-static'
-ALLOWED_HOSTS = ['studyroom.noip.me']
+STATIC_ROOT = '/var/www/backend/static'
+STATIC_URL = '/'
+ALLOWED_HOSTS = ['studyroom.philspark.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'set this in __init__.py',
+        'USER': 'set this in __init__.py',
+        'PASSWORD': 'set this in __init__.py',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
